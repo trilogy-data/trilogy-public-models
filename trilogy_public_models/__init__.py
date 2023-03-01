79cabe7435ac62571e301d2f67a3402fe982e192
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-03-01 10:51:36 -0500

    Add title to SO model

--- trilogy_public_models/__init__.py
+++ trilogy_public_models/__init__.py
@@ -6,7 +6,7 @@ import sys
 
 models: Dict["str", Environment] = {}
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 for loader, module_name, is_pkg in pkgutil.walk_packages([dirname(__file__)]):
     module = loader.find_module(module_name)  # type: ignore
