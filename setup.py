3192ae26c06d950d8ee5b7ae35a03967d480829d
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-04-09 21:31:37 -0400

    Add in NCAA datasets

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
