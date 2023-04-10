f60bd9e69711dba021d2798e8b2c2a48474719e5
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-04-09 21:28:34 -0400

    Add in NCAA datasets

--- trilogy_public_models/inventory.py
+++ trilogy_public_models/inventory.py
@@ -10,10 +10,10 @@ def parse_initial_models(fpath: str) -> Environment:
     files = listdir(dirname(fpath))
 
     for file in files:
-        path = Path(file)
         if file.endswith("entrypoint.preql"):
             with open(join(dirname(fpath), file), "r", encoding="utf-8") as f:
                 contents = f.read()
                 env = Environment(working_path=dirname(fpath))
                 environment, statements = parse(contents, environment=env)
                 return environment
+    raise ValueError("Missing entrypoint.preql")
