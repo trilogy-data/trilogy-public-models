a7c117a82440bbca3fceafe1e48f92a021ce71e1
efromvt <ethan.dickinson@gmail.com>
2023-05-28 15:24:18 -0400

    bump version

--- trilogy_public_models/__init__.py
+++ trilogy_public_models/__init__.py
@@ -6,7 +6,7 @@ import sys
 
 models: Dict["str", Environment] = {}
 
-__version__ = "0.0.11"
+__version__ = "0.0.12"
 
 for loader, module_name, is_pkg in pkgutil.walk_packages([dirname(__file__)]):
     module = loader.find_module(module_name)  # type: ignore
