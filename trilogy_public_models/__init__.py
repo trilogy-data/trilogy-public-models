f96ef1e626505b580d2247163e7349c64d224b2f
efromvt <ethan.dickinson@gmail.com>
2023-09-02 19:36:35 -0400

    build pyinstaller safe version

--- trilogy_public_models/__init__.py
+++ trilogy_public_models/__init__.py
@@ -6,17 +6,27 @@ import sys
 
 models: Dict["str", Environment] = {}
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
-for loader, module_name, is_pkg in pkgutil.walk_packages([dirname(__file__)]):
+
+def load_module_wrap(info: pkgutil.ModuleInfo):
+    loader, module_name, is_pkg = info
     module = loader.find_module(module_name)  # type: ignore
     if not module:
-        continue
+        return None
     _module = module.load_module(module_name)
     try:
         sys.modules["trilogy_public_models." + module_name] = _module.model
         models[module_name] = _module.model
     except AttributeError:
-        continue
+        return None
+
+
+for info in pkgutil.walk_packages([dirname(__file__)]):
+    try:
+        load_module_wrap(info)
+    # this is expected in pyinstaller packages
+    except AttributeError:
+        pass
 
 __all__ = ["models"]
