67d5abfb4b61bb59b259321fcc1ecbe4e15049c0
efromvt <ethan.dickinson@gmail.com>
2024-03-31 12:01:15 -0400

    Improved lazy imports

--- local_examples/imports.py
+++ local_examples/imports.py
@@ -7,13 +7,18 @@ from os.path import dirname
 nb_path = __file__
 root_path = dirname(dirname(nb_path))
 path.insert(0, r"C:\Users\ethan\coding_projects\trilogy-public-models")
-path.insert(0, r"C:\Users\ethan\coding_projects\pypreql")
+
 start = datetime.now()
 from trilogy_public_models import models  # noqa: E402
 from trilogy_public_models.bigquery import stack_overflow  # noqa: E402
 
+
+
 print(type(models["bigquery.stack_overflow"]))
 
-print(type(stack_overflow))
 
-print(datetime.now() - start)
+print(type(stack_overflow))
+print(len(models["bigquery.stack_overflow"].concepts))
+assert len(stack_overflow.concepts) == len(models["bigquery.stack_overflow"].concepts)
+
+print(datetime.now()-start)
