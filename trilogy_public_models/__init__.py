4e801344d31e98ad92caebba5e77831283655dbd
Ethan Dickinson <ethan.dickinson@gmail.com.com>
2024-11-22 22:43:05 -0500

    reduce scale factor

--- trilogy_public_models/__init__.py
+++ trilogy_public_models/__init__.py
@@ -2,7 +2,7 @@ from trilogy_public_models.main import get_executor
 from trilogy_public_models.discovery import data_models
 
 
-__version__ = "0.1.01"
+__version__ = "0.1.02"
 
 
 __all__ = ["data_models", "get_executor"]
