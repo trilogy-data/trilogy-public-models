3608c2ac1b5884825270ed57a5493a6514091798
efromvt <ethan.dickinson@gmail.com.com>
2025-03-03 08:29:43 -0500

    fix up validation

--- trilogy_public_models/validator.py
+++ trilogy_public_models/validator.py
@@ -1,19 +1,21 @@
 from trilogy import Environment, Dialects
 from trilogy.constants import DEFAULT_NAMESPACE
-from trilogy.core.models import (
+from trilogy.authoring import (
     Concept,
-    Datasource,
+    ConceptRef,
     SelectStatement,
-    ProcessedShowStatement,
 )
-from trilogy.core.processing.concept_strategies_v3 import search_concepts
+from trilogy.core.models.datasource import Datasource
+from trilogy.core.statements.execute import ProcessedShowStatement
+from trilogy.core.processing.concept_strategies_v3 import search_concepts, History
 from trilogy.executor import Executor
 from trilogy.parser import parse_text
 from trilogy.core.internal import INTERNAL_NAMESPACE
 from trilogy.core.env_processor import generate_graph
+from trilogy.core.models.build import Factory, BuildConcept, BuildEnvironment
 
 
-def safe_address(input: Concept):
+def safe_address(input: Concept | ConceptRef):
     if input.namespace == DEFAULT_NAMESPACE:
         return input.name
     return input.address
@@ -22,8 +24,6 @@ def safe_address(input: Concept):
 def validate_dataset(
     dataset: Datasource, environment: Environment, executor: Executor, dry_run_client
 ):
-    # TODO: move ths into executor so we don't need this import
-    from google.cloud.bigquery import QueryJobConfig
 
     validation_query = (
         "SELECT\n "
@@ -53,6 +53,9 @@ def validate_dataset(
             # rs = executor.engine.dry_run(compiled_sql)
             if executor.dialect == Dialects.BIGQUERY:
                 # use a dry run to save costs
+                # TODO: move ths into executor so we don't need this import
+                from google.cloud.bigquery import QueryJobConfig
+
                 job_config = QueryJobConfig(dry_run=True, use_query_cache=False)
                 query_job = dry_run_client.query(
                     compiled_sql, job_config=job_config
@@ -82,15 +85,18 @@ def validate_datasource_grain(datasource):
     pass
 
 
-def validate_concept(concept: Concept, env, graph):
+def validate_concept(concept: BuildConcept, history: History, env, graph):
     if concept.namespace == INTERNAL_NAMESPACE or INTERNAL_NAMESPACE in concept.address:
         return
-    search_concepts([concept], environment=env, depth=0, g=graph)
+    search_concepts([concept], history=history, environment=env, depth=0, g=graph)
 
 
 def validate_model(model: Environment, executor: Executor, dry_run_client):
     for dataset in model.datasources.values():
         validate_dataset(dataset, model, executor, dry_run_client)
-    graph = generate_graph(model)
-    for concept in model.concepts.values():
-        validate_concept(concept, model, graph)
+    history = History(base_environment=model)
+    factory = Factory(model)
+    build_model: BuildEnvironment = factory.build(model)
+    graph = generate_graph(build_model)
+    for concept in build_model.concepts.values():
+        validate_concept(concept, history, build_model, graph)
