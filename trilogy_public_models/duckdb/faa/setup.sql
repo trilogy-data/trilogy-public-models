b0e63a51506e801a164580915618607357b6de8e
efromvt <ethan.dickinson@gmail.com.com>
2025-03-26 07:53:49 -0400

    checkpoint faa model

---
+++ trilogy_public_models/duckdb/faa/setup.sql
@@ -0,0 +1,15 @@
+create or replace view airport as SELECT *
+FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/airports.parquet'
+;
+
+create or replace view flight as SELECT *
+FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/flights.parquet'
+;
+
+create or replace view aircraft as SELECT *
+FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/aircraft.parquet'
+;
+
+create or replace view aircraft_model as SELECT *
+FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/aircraft_models.parquet'
+;
