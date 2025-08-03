ffbd3cff806201e7068f8e69eba86208f9516a3e
efromvt <ethan.dickinson@gmail.com.com>
2025-08-03 12:39:30 -0400

    mobs_data_two_tweaks

--- trilogy_public_models/duckdb/mobs/setup.sql
+++ trilogy_public_models/duckdb/mobs/setup.sql
@@ -3,9 +3,29 @@ INSTALL httpfs;
 
 LOAD httpfs;
 
-CREATE OR REPLACE TABLE data_all_112224 AS 
-SELECT * FROM read_csv_auto('https://raw.githubusercontent.com/crmcclain/MOBS_OPEN/refs/heads/main/data_all_112224.csv',
-sample_size=-1);
+CREATE OR REPLACE TABLE data_all_112224 AS
+SELECT
+    column00,
+    Order_Added,
+    valid_aphiaID,
+    phylum,
+    class,
+    "order",
+    family,
+    genus,
+    scientificName,
+    scientificNameAuthorship,
+CASE WHEN length_cm = 'NA' then cast(null as float) else cast(length_cm as float) END length_cm,
+    diameter_width_cm,
+CASE WHEN height_cm = 'NA' then cast(null as float) else cast(height_cm as float) END height_cm,
+    Notes,
+    Size_Ref,
+    Date_Added
+FROM read_csv_auto(
+    'https://raw.githubusercontent.com/crmcclain/MOBS_OPEN/refs/heads/main/data_all_112224.csv',
+    sample_size = -1
+);
+
 
 
 CREATE OR REPLACE TABLE  genus_info AS 
