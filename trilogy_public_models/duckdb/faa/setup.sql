5d8c60ad5e7ba7a5fdbabcffa2e43d96f7ce5bda
efromvt <ethan.dickinson@gmail.com.com>
2025-09-04 20:48:02 -0400

    automated validation fixes

--- trilogy_public_models/duckdb/faa/setup.sql
+++ trilogy_public_models/duckdb/faa/setup.sql
@@ -1,20 +1,20 @@
-create or replace view airport as SELECT *
+create or replace table airport as SELECT *
 FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/airports.parquet'
 ;
 
-create or replace view flight as SELECT *
+create or replace table flight as SELECT *
 FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/flights.parquet'
 ;
 
 
-create or replace view aircraft as SELECT *
+create or replace table aircraft as SELECT *
 FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/aircraft.parquet'
 ;
 
-create or replace view aircraft_model as SELECT *
+create or replace table aircraft_model as SELECT *
 FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/aircraft_models.parquet'
 ;
 
-create or replace view carrier as SELECT *
+create or replace table carrier as SELECT *
 FROM 'https://raw.githubusercontent.com/malloydata/malloy-samples/refs/heads/main/data/carriers.parquet'
 ;
